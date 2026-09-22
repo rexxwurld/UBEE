ad08e7b29d69b4d2fbe154bcfb2651caef44ed4a
Emmanuel Edidiong Noah <rexxwurld@gmail.com>
2026-09-22 15:08:21 +0100

    Rename namespace from 'mobile' to 'UBee.App'

--- mobile/Platforms/Android/MainActivity.cs
+++ mobile/Platforms/Android/MainActivity.cs
@@ -2,7 +2,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.OS;
 
-namespace mobile
+namespace UBee.App
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
